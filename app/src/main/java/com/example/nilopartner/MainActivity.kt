eb5cbbc71631b571ce42c03d0c7ef2463cac4de7
Josue <sjosue5082002@gmail.com>
2022-01-11 20:02:57 -0600

    Retener sesión activa

--- app/src/main/java/com/example/nilopartner/MainActivity.kt
+++ app/src/main/java/com/example/nilopartner/MainActivity.kt
@@ -9,24 +9,49 @@ import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var authStateListener: FirebaseAuth.AuthStateListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val providers = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build()) //son los email de la Auth
+        configAuth()
+    }
 
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-            val response = IdpResponse.fromResultIntent(it.data)
+    private fun configAuth(){
+        firebaseAuth = FirebaseAuth.getInstance()
+        authStateListener = FirebaseAuth.AuthStateListener { auth ->
+            if (auth.currentUser != null){ //saber si el usuario ya esta logeado
+                supportActionBar?.title = auth.currentUser?.displayName
+            }else{
+                val providers = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build()) //son los email de la Auth
 
-            if (it.resultCode == RESULT_OK){ //ver si existe un usuario Autenticado
-                val user = FirebaseAuth.getInstance().currentUser //variable para el user Auth
-                if (user != null){
-                    Toast.makeText(this,"Bienvenido",Toast.LENGTH_SHORT).show()
-                }
+                registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+                    val response = IdpResponse.fromResultIntent(it.data)
+
+                    if (it.resultCode == RESULT_OK){ //ver si existe un usuario Autenticado
+                        val user = FirebaseAuth.getInstance().currentUser //variable para el user Auth
+                        if (user != null){
+                            Toast.makeText(this,"Bienvenido",Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }.launch(AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .build())
             }
-        }.launch(AuthUI.getInstance()
-            .createSignInIntentBuilder()
-            .setAvailableProviders(providers)
-            .build())
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        firebaseAuth.addAuthStateListener(authStateListener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        firebaseAuth.removeAuthStateListener(authStateListener)
     }
 }
