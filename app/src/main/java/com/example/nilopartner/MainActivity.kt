fe81105bdbe5329c62389a2b2c81dc3b788de1fc
Josue <sjosue5082002@gmail.com>
2022-01-11 19:54:43 -0600

    Inicio de sesión con FirebaseUI Auth

--- app/src/main/java/com/example/nilopartner/MainActivity.kt
+++ app/src/main/java/com/example/nilopartner/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.nilopartner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val providers = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build()) //son los email de la Auth
+
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            val response = IdpResponse.fromResultIntent(it.data)
+
+            if (it.resultCode == RESULT_OK){ //ver si existe un usuario Autenticado
+                val user = FirebaseAuth.getInstance().currentUser //variable para el user Auth
+                if (user != null){
+                    Toast.makeText(this,"Bienvenido",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }.launch(AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build())
     }
 }
