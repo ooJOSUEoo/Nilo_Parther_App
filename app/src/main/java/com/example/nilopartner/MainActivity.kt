ec5061b929287739d293245b33c6c23b453e371c
Josue <sjosue5082002@gmail.com>
2022-01-11 20:37:48 -0600

    Control de vistas y retroceso.

--- app/src/main/java/com/example/nilopartner/MainActivity.kt
+++ app/src/main/java/com/example/nilopartner/MainActivity.kt
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import com.example.nilopartner.databinding.ActivityMainBinding
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding //llamar a binding
 
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var authStateListener: FirebaseAuth.AuthStateListener
@@ -23,12 +27,18 @@ class MainActivity : AppCompatActivity() {
             if (user != null){
                 Toast.makeText(this,"Bienvenido",Toast.LENGTH_SHORT).show()
             }
+        }else{
+            if (response == null) { //el usuario al pulsado hacia atras
+                Toast.makeText(this,"Hasta pronto",Toast.LENGTH_SHORT).show()
+                finish() //cierra la app
+            }
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater ) //inicializar binding
+        setContentView(binding.root)
 
         configAuth()
     }
@@ -38,6 +48,7 @@ class MainActivity : AppCompatActivity() {
         authStateListener = FirebaseAuth.AuthStateListener { auth ->
             if (auth.currentUser != null){ //saber si el usuario ya esta logeado
                 supportActionBar?.title = auth.currentUser?.displayName
+                binding.tvInit.visibility = View.VISIBLE
             }else{
                 val providers = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build()) //son los email de la Auth
 
@@ -70,6 +81,13 @@ class MainActivity : AppCompatActivity() {
                 AuthUI.getInstance().signOut(this)
                     .addOnSuccessListener {
                         Toast.makeText(this,"Sesión terminada.",Toast.LENGTH_SHORT).show()
+                    }
+                    .addOnCompleteListener {
+                        if (it.isSuccessful){//si es exitoso la vista se quitara
+                            binding.tvInit.visibility = View.GONE
+                        }else{
+                            Toast.makeText(this,"No se pudo cerrar la sesión.",Toast.LENGTH_SHORT).show()
+                        }
                     }
             }
         }
