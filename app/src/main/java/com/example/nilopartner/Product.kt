314be2c6c553dd5128234857528dc0e7b3ed2e62
Josue <sjosue5082002@gmail.com>
2022-01-12 13:57:20 -0600

    Data class Product

---
+++ app/src/main/java/com/example/nilopartner/Product.kt
@@ -0,0 +1,26 @@
+package com.example.nilopartner
+
+
+data class Product( //clase para el modelo de datos de los producto
+        var id: String?, //id
+        var name: String?, //nombre
+        var description: String?, //descripcion
+        var imgUrl: String?, //url de img
+        var quantity: Int = 0, // cantidad
+        var price: Double = 0.0){ //precio
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Product
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id?.hashCode() ?: 0
+    }
+}
